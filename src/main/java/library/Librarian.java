a3d3252e078f66d60483bcb5797c028ef1065698
Alexander Slepchenko <asander@list.ru>
2023-12-04 18:16:43 +0300

    Добавлен форматер для отображения дат.

--- src/main/java/library/Librarian.java
+++ src/main/java/library/Librarian.java
@@ -3,6 +3,8 @@ package library;
 import library.model.Book;
 import library.model.BorrowedBook;
 
+import java.time.format.DateTimeFormatter;
+
 public class Librarian {
 
     public String forfeitMessage(int forfeitCount) {
@@ -15,15 +17,16 @@ public class Librarian {
 
     public String receipt(BorrowedBook borrowedBook, Book book, String userName) {
         StringBuilder message = new StringBuilder();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         message.append("Квитанция для получения книги на имя: ")
                 .append(userName)
                 .append(System.lineSeparator())
                 .append("Название книги: ").append(book.getName()).append(System.lineSeparator())
-                .append("Сроком до: ").append(borrowedBook.getRefundDate()).append(System.lineSeparator())
+                .append("Сроком до: ").append(borrowedBook.getRefundDate().format(formatter)).append(System.lineSeparator())
                 .append("Внесенный залог: ").append(borrowedBook.getDeposit()).append(" рублей").append(System.lineSeparator())
                 .append("Внесенная сумма аренды книги: ").append(borrowedBook.getRental()).append(" рублей").append(System.lineSeparator())
                 .append(System.lineSeparator()).append(System.lineSeparator())
-                .append("Библиотекарь Слепченко Александр Сергеевич ").append(borrowedBook.getBorrowDate());
+                .append("Библиотекарь Слепченко Александр Сергеевич ").append(borrowedBook.getBorrowDate().format(formatter));
         return message.toString();
     }
 
